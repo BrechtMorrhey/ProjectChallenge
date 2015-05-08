bc237d249ae8cd126d459999e3e803521d7b9908
BrechtMorrhey <brechtabc@hotmail.com>
2015-05-08 15:17:44 +0200

    BestandTeGrootException

---
+++ BestandTeGrootException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectChallenge
+{
+    class BestandTeGrootException: ApplicationException
+    {
+         public BestandTeGrootException() { }
+         public BestandTeGrootException(string message)
+            : base(message)
+        {          
+        }
+    }
+}
